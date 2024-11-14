bf96a53e580de424d80cb176d669206ecd4496cc
Murat Okutucu <mokutucu@c3a6c1.42berlin.de>
2024-11-14 17:05:49 +0100

    initialwindowrender

---
+++ cube3d/include/cube3d.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:20:48 by mokutucu          #+#    #+#             */
+/*   Updated: 2024/11/14 17:03:54 by mokutucu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+
+#include "../libs/libft/libft.h"
+#include "../libs/minilibx-linux/mlx.h"
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_img
+{
+    void *mlx;
+    void *mlx_win;
+    void *img;
+    char *img_address;
+    int     bpp;
+    int     line_length;
+    int     endian;
+} t_img;
+
+typedef struct s_player
+{
+    int player_id;
+    
+} t_player;
+
+typedef struct s_game 
+{
+    t_img img;
+    t_player player;
+    
+} t_game;
+
+#endif
---
+++ cube3d/src/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:16:09 by mokutucu          #+#    #+#             */
+/*   Updated: 2024/11/14 17:05:00 by mokutucu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cube3d.h"
+
+void    render(t_game *game)
+{
+    game->img.mlx = mlx_init();
+    game->img.mlx_win = mlx_new_window(game->img.mlx, 800, 600, "cub3d");
+    game->img.img = mlx_new_image(game->img.mlx, 800, 600);
+    game->img.img_address = mlx_get_data_addr(game->img.img, &game->img.bpp,
+			    &game->img.line_length,  &game->img.endian);
+    mlx_put_image_to_window(game->img.mlx, game->img.mlx_win, game->img.img, 0, 0);
+    mlx_loop(game->img.mlx);
+}
+int main(void)
+{
+    t_game game;
+    
+    render(&game);
+    
+    return 0;
+}
